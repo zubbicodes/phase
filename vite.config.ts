a6293d0db428a99ebfd693a5ac80fed4c58f188b
Zaryab Mahmood <zubbicodes>
2025-06-21 05:13:35 +0500

    Optimized Launch

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
